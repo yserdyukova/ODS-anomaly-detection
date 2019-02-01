51fd51f5bf47ef26257da9187d55d02ffef18e78
yserdyukova <jserdukova@mail.ru>
2019-02-01 11:09:20 +0300

    ...

---
+++ server/httpsesrver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
